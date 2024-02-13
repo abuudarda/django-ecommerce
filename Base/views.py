b1c85a34fb9a7e850c1726323379f144be888f36
abuudarda <abu.darda@g.bracu.ac.bd>
2024-02-13 11:37:16 +0600

    chore: static fixes

--- Base/views.py
+++ Base/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'Base/index.html')
+
+def contact(request):
+    return render(request, 'Base/contact.html')
